569cf6c83333464819c986c780d2f01353ec4653
delowar hossain <bscdelowar@yahoo.com>
2026-02-21 15:56:09 +0600

    cors

--- config/cors.php
+++ config/cors.php
@@ -2,8 +2,6 @@
 return [
   'paths' => ['api/*', 'login', 'register'],
   'allowed_methods' => ['*'],
-  'allowed_origins' => ['http://localhost:3000','http://127.0.0.1:3000','http://localhost:3001','http://localhost:3002',],
-  'allowed_headers' => ['*'],
   'supports_credentials' => false, // token mode => false
 ];
 
